1c4420c76c00f0838206712960e5366d58543f05
Marina Nobre <mahnobre06@gmail.com>
2025-05-04 19:16:30 -0300

    Add files via upload

---
+++ sistemadenumeraçãoatv.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main (){
+ char binario[100]
+ int decimal = 0;
+ int quantd = strlen(binario);
+
+ printf("digite o numero binário: ");
+ scanf("%s", &binario);
+
+ for (int i = 0; i < tamanho; i++) {
+ if (binario[quantd - 1 - i] == '1') {
+ decimal += pow(2, i); }
+ }
+
+ printf("O número em decimal é: %d", decimal);
+ 
+return 0;
+
+
+}
